f7f879c1681821c9f12ff65c336597dc8fbd1eab
DadeCountyCoder <dadecountycoder@gmail.com>
2020-12-01 12:53:30 -0500

    setting todos and creating buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from './TodoList'
 
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
-    <TodoList />
+    <>
+      <TodoList todos/>
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
   )
    
 }
