f13a880c6a73a759fd643f609a72318545108ea5
DadeCountyCoder <dadecountycoder@gmail.com>
2020-12-01 13:14:55 -0500

    create checkbox

--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Todo({todo}) {
     return (
         <div>
-            {todo}
+            <label>
+                <input type="checkbox">
+            {todo.name}
+            </label>
         </div>
     )
 }
