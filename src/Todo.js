bf67a61ae779e7f350263a3cf4a54d9de227d74b
DadeCountyCoder <dadecountycoder@gmail.com>
2020-12-01 13:19:43 -0500

    empty array to add todo

--- src/Todo.js
+++ src/Todo.js
@@ -4,7 +4,7 @@ export default function Todo({todo}) {
     return (
         <div>
             <label>
-                <input type="checkbox">
+                <input type="checkbox" checked={todo.complete} />
             {todo.name}
             </label>
         </div>
